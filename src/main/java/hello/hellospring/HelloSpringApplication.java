66b29e393e80d3f823d441cee037858cce2ac7f5
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-09-07 23:08:37 +0900

    style: add index.html

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // 메인 메서드로부터 모든 것이 시작됨
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
